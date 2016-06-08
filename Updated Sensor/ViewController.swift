a91666a6eb9ca4616b1784bf59be5e43c94a822a
Sharabesh Ramesh <sharabesh@berkeley.edu>
2016-06-07 22:23:44 -0400

    Initial Commit

---
+++ Updated Sensor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Updated Sensor
+//
+//  Created by Sharabesh Ramesh on 6/7/16.
+//  Copyright © 2016 Sharabesh Ramesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Updated SensorTests/Updated_SensorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Updated_SensorTests.swift
+//  Updated SensorTests
+//
+//  Created by Sharabesh Ramesh on 6/7/16.
+//  Copyright © 2016 Sharabesh Ramesh. All rights reserved.
+//
+
+import XCTest
+@testable import Updated_Sensor
+
+class Updated_SensorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
